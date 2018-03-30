81b6989c98d5629bb5392832088932df11990748
huxingguo <759478185@qq.com>
2018-03-30 10:08:49 +0800

    规范注释

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
-class Promise{
-    constructor(executor) {//executor 执行器函数
+/**
+ * 
+ * 
+ * @class Promise
+ */
+class Promise {
+    constructor(executor) { //executor 执行器函数
         this.status = 'pending';
         this.value = null;
         this.rejectReson = null;
@@ -9,35 +14,39 @@ class Promise{
         this.resolve = this.resolve.bind(this);
         this.then = this.then.bind(this);
         this.catch = this.catch.bind(this);
-        try{
-
+        try {
             executor(this.resolve, this.reject);
-        }catch(e){
+        } catch (e) {
             this.reject(e);
         }
     }
-    /*@parmas value anyType
-    ** return null
-    */
+    /**
+     * 
+     * 
+     * @param {Any} value
+     * @return {Null}
+     */
     resolve(value) {
         //保证在pending状态时,在push callback之后调用
         setTimeout(_ => {
-          if(this.status === 'pending'){
+            if (this.status === 'pending') {
                 this.status = 'resolved';
                 this.value = value;
-                if(this.onFulfilledCallBack && Object.prototype.toString.call(this.onFulfilledCallBack) === '[object Function]'){
+                if (this.onFulfilledCallBack && Object.prototype.toString.call(this.onFulfilledCallBack) === '[object Function]') {
                     this.onFulfilledCallBack(this.value);
                 }
             }
-        });        
+        });
     }
-    /*
-    **@parmas rejectReson anyType
-    **return null
-    */
+    /**
+     * 
+     * 
+     * @param  {Any} rejectReson
+     * @return {Null}
+     */
     reject(rejectReson) {
         setTimeout(_ => {
-            if(this.status === 'pending'){
+            if (this.status === 'pending') {
                 this.status = 'rejected';
                 this.rejectReson = rejectReson;
                 if (this.onRejectedCallBack && Object.prototype.toString.call(this.onRejectedCallBack) === '[object Function]') {
@@ -46,91 +55,109 @@ class Promise{
             }
         });
     }
-    /*
-    **@params onFullfilled function
-    **@params onRejected function
-    **return promise
-    */
-    then(onFulfilled, onRejected){
+    /**
+     * 
+     * 
+     * @param {Function} onFullfilled 
+     * @param {Function} onRejected 
+     * @return {Promise}
+     */
+    then(onFulfilled, onRejected) {
         const _this = this;
-        return new Promise(function(resolve, reject){
+        return new Promise(function (resolve, reject) {
             //异步操作已经完成，直接调用
             if (_this.status === 'resolved') {
-                try{
+                try {
                     resolve(onFulfilled(value));
-                }catch(e){
+                } catch (e) {
                     reject(e);
                 }
             }
             if (_this.status === 'rejected') {
-                try{
+                try {
                     reject(onRejected(_this.rejectReson));
-                }catch(e){
+                } catch (e) {
                     reject(e);
                 }
             }
             //未完成异步,等待回调
-            if(_this.status === 'pending'){
+            if (_this.status === 'pending') {
                 _this.onFulfilledCallBack = onFulfilled ? (value => {
-                    try{
+                    try {
                         resolve(onFulfilled(value));
-                    }catch(e){
+                    } catch (e) {
                         reject(e);
                     }
-                  
+
                 }) : null;
-                _this.onRejectedCallBack= (reson => {
-                    if(onRejected){
-                        try{
+                _this.onRejectedCallBack = (reson => {
+                    if (onRejected) {
+                        try {
                             reject(onRejected(reson) || _this.rejectReson);
-                        }catch(e){
+                        } catch (e) {
                             reject(e);
                         }
-                    }else{
+                    } else {
                         reject(_this.rejectReson)
                     }
                 });
             }
         });
     }
-    /*@params onFulfilled function
-    **return promise
-    */
-    catch(onFulfilled){
+    /**
+     * 
+     * 
+     * @param {Function} onFulfilled 
+     * @return {Promise}
+     */
+    catch (onFulfilled) {
         return this.then(null, onFulfilled);
     }
-    /*@params value anyType
-    **return promise
-    */
-    static resolve(value){
-        return new Promise(function (resolve, reject){
+    /**
+     * 
+     * 
+     * @static
+     * @param {Any} value 
+     * @return {Promise}
+     */
+    static resolve(value) {
+        return new Promise(function (resolve, reject) {
             resolve(value);
         })
     }
-    /*@params value anyType
-    **return promise
-    */
-    static reject(value){
+    /**
+     * 
+     * 
+     * @static
+     * @param {Any} value 
+     * @return {Promise}
+     */
+    static reject(value) {
         return new Promise(function (resolve, reject) {
             reject(value);
         })
     };
-    /*@params promises array[promise]
-    **return promise
-    */
-    static all(promises){
-        return new Promise(function (resolve, reject){
+    /**
+     * 
+     * 
+     * @static
+     * @param {Array[Promise]} promises 
+     * @returns {Promise}
+     */
+    static all(promises) {
+        return new Promise(function (resolve, reject) {
             let result = [];
             let i = 0;
-            function pushResult(index, value){
+
+            function pushResult(index, value) {
                 result[index] = value;
                 i++;
-                if(i === promises.length){
+                if (i === promises.length) {
                     resolve(result);
                 }
             }
             promises.forEach((promise, index) => {
-                promise.then(value =>{
+                promise.then(value => {
                     pushResult(index, value);
                 }).catch(e => {
                     reject(e);
@@ -138,10 +165,14 @@ class Promise{
             });
         })
     }
-    /*@params promises array[promise]
-    **return promise
-    */
-    static race(promises){
+    /**
+     * 
+     * 
+     * @static
+     * @param {Array[Promise]} promises 
+     * @return {Promise} 
+     */
+    static race(promises) {
         return new Promise(function (resolve, reject) {
             promises.forEach(promise => {
                 promise.then(value => {
@@ -161,5 +192,3 @@ if (typeof window !== 'undefined') {
 if (typeof global !== 'undefined') {
     global.Promise = Promise;
 }
-
-
--- test.js
+++ test.js
@@ -1,31 +1,41 @@
-require('./index')
+// require('./index')
 
 
-const b = new Promise(function (resolve, reject) {
-    resolve('ff');
-}).then(function (value) {
-    throw new Error('33')
-    return 111
-}).catch(e =>{
-    console.log(e.message)
-})
-// const a = new Promise(function(resolve, reject){
-//     resolve('error');
-// }).then(function(value){
-   
-//     return 222
-// })
-
-
-// Promise.all([a, b]).then(i =>{
-//     console.log(i, '-----')
+// const b = new Promise(function (resolve, reject) {
+//     resolve('ff');
+// }).then(function (value) {
+//     throw new Error('33')
+//     return 111
 // }).catch(e =>{
-//     console.log(e)
+//     console.log(e.message)
 // })
+// // const a = new Promise(function(resolve, reject){
+// //     resolve('error');
+// // }).then(function(value){
+
+// //     return 222
+// // })
+
+
+// // Promise.all([a, b]).then(i =>{
+// //     console.log(i, '-----')
+// // }).catch(e =>{
+// //     console.log(e)
+// // })
 
 
 
+function* genFun1() {
+    yield 2;
+    yield 3;
+    yield 4;
+}
 
-
-
-
+function* genFun2() {
+    yield 1;
+    yield genFun1;
+    yield 5;
+}
+for (var i of genFun2()) {
+    console.log(i);
+}
